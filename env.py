aa103972a252b4a85c13b1ffed9be00f59a3accd
Ubuntu <arobotadmin@rbtimgdev.clr2z5w4njyu3b1zy2cnkdwbwd.hx.internal.cloudapp.net>
2018-01-18 09:29:35 +0000

    Initial commit

---
+++ env.py
@@ -0,0 +1,14 @@
+import cv2
+import scipy
+import numpy as np
+import tensorflow as tf
+import keras
+
+import sys
+import os
+
+print('cv2', cv2.__version__)
+print('tensorflow', tf.__version__)
+print('python', sys.version)
+print('numpy', np.__version__)
+print('scipy', scipy.__version__)
